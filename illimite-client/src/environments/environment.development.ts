1fb2458abb11c0c8dfd045fb5c5bb97e16ecf211
Anandhu M V <anandhumv012@gmail.com>
2026-06-03 12:57:26 +0530

    chore: configure live Firebase credentials in environments

--- illimite-client/src/environments/environment.development.ts
+++ illimite-client/src/environments/environment.development.ts
@@ -1,11 +1,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'mock-api-key-illimite-12345',
-    authDomain: 'illimite-mock-project.firebaseapp.com',
-    projectId: 'illimite-mock-project',
-    storageBucket: 'illimite-mock-project.appspot.com',
-    messagingSenderId: '123456789012',
-    appId: '1:123456789012:web:mockappid123456'
+    apiKey: 'AIzaSyBkuwnSnT9HDbwTR6EJhZBzs4QNJLQvVRc',
+    authDomain: 'illimite-ec139.firebaseapp.com',
+    projectId: 'illimite-ec139',
+    storageBucket: 'illimite-ec139.firebasestorage.app',
+    messagingSenderId: '860916037724',
+    appId: '1:860916037724:web:267e1488b1c83a18054e99',
+    measurementId: 'G-8519580QV9'
   }
 };
